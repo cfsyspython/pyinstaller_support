2b6849f9b942eb4622aa6a5a8542024a037fe813
cfsyspython <pyteam@cfsys.xyz>
2024-06-08 13:46:49 +0530

    Update get py file.py
    correct format of data files

--- get py file.py
+++ get py file.py
@@ -3,9 +3,11 @@ import os
 def find_all_py_files(directory):
     py_files = []
     for root, _, files in os.walk(directory):
-        if ".git" in root or ".idea" in root:
+        if ".git" in root or ".idea" in root or "temp" in root or ".venv" in root:
             continue
         for file in files:
+            if file=="List_of_py&nonpy_files.py":
+                continue
             if file.endswith('.py'):
                 # Remove the base directory path from the file path
                 relative_path = os.path.relpath(os.path.join(root, file), directory)
@@ -24,22 +26,33 @@ def find_all_py_files(directory):
 def find_all_non_py_files(directory):
     non_py_files = []
     for root, _, files in os.walk(directory):
-        if ".git" in root or ".idea" in root:
+        if ".git" in root or ".idea" in root or "temp" in root or  ".venv" in root or  "build" in root or "dist" in root:
             continue
         for file in files:
+            if file=="main.spec" or file=="piSetting.txt" or file=="requirements.txt" or file=="test.txt" or file==".gitignore":
+                continue
             if not file.endswith('.py'):
                 # Remove the base directory path from the file path
                 relative_path = os.path.relpath(os.path.join(root, file), directory)
                 # Normalize the path
-                a=relative_path.split("\\")
-                new=""
+                print(root,"\n",_,"\n",file,"\n\n")
+                a = root.split("\\")
+                new = ""
                 for i, j in enumerate(a):
-                    if i>0:
-                        new+="/"+j
+                    if i > 0:
+                        new += "/" + j
                     else:
-                        new+=j
+                        new += j
+                dire=""
+                folder=root.split("py_treding")[1].split("\\")
+                print(folder)
+                for i, j in enumerate(folder):
 
-                non_py_files.append(new)
+                    if i > 0:
+                        dire += "/" + j
+                    else:
+                        dire += j
+                non_py_files.append(eval(f'''("{new}/{file}","{dire}")'''))
     return non_py_files
 
 # Specify the directory to search
@@ -47,8 +60,8 @@ directory_to_search = os.getcwd().split('temp')[0]
 
 # Find all Python files
 python_files = find_all_py_files(directory_to_search)
-print(python_files)
+print("py: ",python_files)
 
 # Find all non-Python files
 non_py_files = find_all_non_py_files(directory_to_search)
-print(non_py_files)
+print("nonpy: ",non_py_files)
